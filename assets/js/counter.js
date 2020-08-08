210899bed1655c05afe14d6c4306d63fdec12619
Madison Hardt <madison.hardt@hey.com>
2020-08-08 08:15:02 -0500

    prefixed 0s to counter

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -9,18 +9,32 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
+Number.prototype.pad = function(size) {
+  var s = String(this);
+  if (s.length < (size || 2)) {
+    s = "0" + s;
+    return s;
+  }
+  else {
+    return s;
+  }
+}
+
+
 let countDown = new Date('Mar 13, 2020 00:00:00').getTime(),
     x = setInterval(function() {    
 
-      let now = new Date().getTime(),
-        distance = now - countDown;
+      var now = new Date().getTime();
+      var distance = now - countDown;
 
         document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+        document.getElementById('hours').innerText = (Math.floor((distance % (day)) / (hour))).pad(2),
+        document.getElementById('minutes').innerText = (Math.floor((distance % (hour)) / (minute))).pad(2),
+        document.getElementById('seconds').innerText = (Math.floor((distance % (minute)) / second)).pad(2);
 
     }, second)
+
+
 
 // rotate on scroll
 window.onscroll = function () {
